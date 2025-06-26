e1f628a7bd1fcf8f6c52b8dd7b4ee35538ada8c7
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-06-26 17:55:23 +0100

    feat: implement routes that correspond to Users controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :users , only: [:new, :create, :index, :login, :signup]
+  resources :posts , only: [:show ]
+  get "/", to: "users#index"
+  get "/users/", to: "users#index"
+  get "/users/:name", to: "users#name"
+  get "/login", to: "users#new"
+  get "/signup", to: "users#register"
+  post "/login", to: "users#login", as: "perform_login"
+  post "/signup", to: "users#signup", as: "perform_signup"
+  
   # Defines the root path route ("/")
   # root "posts#index"
 end
