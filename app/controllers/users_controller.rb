04df8823062f413c016db97badf83123f0002265
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-06-27 00:56:01 +0100

    fix: remove the signup and login logic from the Users Controller to Sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,18 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:signup, :login]
+  # skip_before_action :verify_authenticity_token, only: [:signup, :login]
+  before_action :require_login
 
   def index
     @users = User.all
     render json: @users
   end
-  
+
   def name
     user_params = params.extract_value(:name)
     render json: user_params
   end
   # Direct to the Login Form
-  def new
-    @user = User.new
-  end
-# Direct to the Register/Sign Up form
-  def register
-    @user = User.new
-  end
-
-  def login
-    @user = User.new(allowed_params)
-    @existing_user = User.find_by(name: @user.name);
-    if @existing_user
-      redirect_to post_path(@existing_user.id)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def signup
-    @user = User.new(allowed_params)
-    existing_user = User.find_by(name: @user.name) || User.find_by(email: @user.email)
   
-    if existing_user
-      render :register, status: :unprocessable_entity
-    else
-      if @user.save
-        redirect_to post_path(@user.id)
-      else
-        render :register, status: :unprocessable_entity # <== this line was missing
-      end
-    end
-  end  
 
   def create
     @user = User.new(allowed_params)
@@ -51,7 +21,7 @@ class UsersController < ApplicationController
   private
 
   def allowed_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
