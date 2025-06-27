ad92c33d295492241a0170d177bcaa8f968bf29a
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-06-27 01:02:25 +0100

    update: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_25_110632) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_26_191747) do
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
@@ -32,6 +32,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_25_110632) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "password"
+    t.string "remember_digest"
+    t.string "remember_token"
   end
 
   add_foreign_key "comments", "posts"
