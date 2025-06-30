8589603b60bbe98451b864af57ab0c7a239aeccf
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-06-30 18:58:57 +0100

    feat: configure diff formats for showing the email to the default mailer.html.erb as html and to user.html.erb as text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,15 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @greeting = "Hi"
     @user = params[:user]
+
     mail(
       to: @user.email,
       subject: "#{@greeting} - Welcome to My Awesome Site!"
-    )
+    ) do |format|
+      # Render to mailer.html.erb in app/views/layout by default
+      format.html
+      # Render to user.html.erb in app/views/layout as text
+      format.text { render text: "user" }
+    end
   end
 end
